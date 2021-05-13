0cdf1db7a80ea0e6cacc9cfef865af8f12330bed
Marita Lasmane <marita.lasmane@gmail.com>
2021-05-13 22:21:46 +0300

    Updated version of task 5.3

--- task_5.3._ML.py
+++ task_5.3._ML.py
@@ -9,23 +9,54 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-lat = input('Enter latitude: ')
-lon = input('Enter longitude: ')
+while True:
+    lat = input('Enter latitude (range -90 to 90): ')
+    lon = input('Enter longitude (range -180 to 180): ')
 
-url = serviceurl + urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': osm_format})
-print('Calling: ', url)
+    url = serviceurl + urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': osm_format})
+    print('Calling: ', url)
 
-uh = urllib.request.urlopen(url, context=ctx)
-data = uh.read().decode()
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
 
-try:
-    js = json.loads(data)
-except:
-    js = None
-    
-if not js or 'features' not in js:
-    print('==== Failure To Retrieve ====')
-    print(data)
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+        
+    if not js or 'features' not in js:
+        print('Something went wrong:')
+        print(data)
+    else:
+        try:
+            name = js['features'][0]['properties']['name']
+        except:
+            name = 'None'
+            pass
+        try:
+            type = js['features'][0]['properties']['type']
+        except:
+            type = 'None'
+            pass
+        try:
+            country_name = js['features'][0]['properties']['address']['country']
+        except:
+            country_name = 'None'
+            pass
+        try:
+            country_code = js['features'][0]['properties']['address']['country_code']
+        except:
+            country_code = 'None'
+            pass
+        print('Place name:', name, '\nType of the place:', type, '\nCountry:', country_name, '(',country_code.upper(),')')
 
-location = js['features'][0]['properties']['display_name']
-print(location)
+    while True:
+        repeat = input('Repeat (Y/N)?')
+
+        if repeat.upper() == 'Y':
+            break
+        elif repeat.upper() == 'N':
+            exit()
+        else:
+            print('Unsupported input')
+            continue
