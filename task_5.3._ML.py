7df17e18d5ce88bd931df23cab2ae60f9b3e496a
Marita Lasmane <marita.lasmane@gmail.com>
2021-05-13 13:41:04 +0300

    Cleanup

--- task_5.3._ML.py
+++ task_5.3._ML.py
@@ -9,22 +9,23 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    lat = input('Enter latitude: ')
-    lon = input('Enter longitude: ')
+lat = input('Enter latitude: ')
+lon = input('Enter longitude: ')
 
-    url = serviceurl + urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': osm_format})
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    print('Retrieved', len(data), 'characters')
-    try:
-        js = json.loads(data)
-    except:
-        js = None
-    if not js or 'features' not in js:
-        print('==== Failure To Retrieve ====')
-        print(data)
-        continue
-    location = js['features'][0]['properties']['display_name']
-    print(location)
+url = serviceurl + urllib.parse.urlencode({'lat': lat, 'lon': lon, 'format': osm_format})
+print('Calling: ', url)
+
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read().decode()
+
+try:
+    js = json.loads(data)
+except:
+    js = None
+    
+if not js or 'features' not in js:
+    print('==== Failure To Retrieve ====')
+    print(data)
+
+location = js['features'][0]['properties']['display_name']
+print(location)
